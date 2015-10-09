c0fbe5a0538d87e0558abe8e6bc3ec4969be9492
hanh368 <hanh368@newschool.edu>
2015-10-09 18:41:03 +0000

    test

---
+++ scripts.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+console.log("****************");
+
+var symbols = ["*", "$", "#"];
+
+var pattern = "*";
+
+// for(var increment = 1; increment<20; increment++) {
+//   console.log("*");
+//   pattern = pattern + symbols[increments];
+// }
