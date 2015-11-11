54dec916435d4d44f33c8d1c047f4b87c2d6f9d6
hanh368 <hanh368@newschool.edu>
2015-11-11 20:15:08 +0000

    Adding index file

--- jquery_exercise/js/script.js
+++ jquery_exercise/js/script.js
@@ -25,6 +25,20 @@ Now that we have stored the status of our button, let's show the user an image i
 If the user has activated the button, toggle the visibility of id "cat" to reveal our animated leopard.
 
 */
+var clicked = false;
+
 $("#pirate").remove();
-
-
+$("body").addClass("gradient");
+$("#toggle").click(function() {
+    if (clicked ==false) {
+$("#status").html("GO").css("background-color","green");
+$("#toggle").html("STOP");
+clicked = true;
+} else {
+    $("#status").html("STOP");
+$("#status").css("background-color","RED");
+$("#toggle").html("START");
+clicked = false;
+    
+}
+});
