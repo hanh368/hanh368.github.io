054e42ec0acdb870411822446b909ff994b26290
hanh368 <hanh368@newschool.edu>
2015-11-11 20:06:01 +0000

    Adding index file

---
+++ myscript.js
@@ -0,0 +1,12 @@
+$("body").css("background-color","blue")
+
+function myFunction() {
+    //do stuff
+    console.log("hello world");
+}
+
+function addNums(num1, num2){
+    var sum = num1+num2;
+    console.log(sum);
+}
+
--- scripts.js
+++ scripts.js
@@ -1,11 +1,33 @@
-console.log("hello world");
-console.log("****************");
+// console.log("hello world");
+// console.log("****************");
 
-var symbols = ["*", "$", "#"];
+// var symbols = ["*", "$", "#"];
+
+// var pattern = "*";
+
+// for(var increment = 0; increment<20; increment++) {
+//   console.log("*");
+//   pattern = pattern + symbols[increments];
+//   var ampersand ="&";
+//   if (increment > 5);
+//   for (var another = 0; another <10; )
+// }
+console.log("hello world");
+console.log("*************");
+
+var symbols = ["*","$","#","@","*","$","#","@","*","$","#","@","*","$","#","@"]
 
 var pattern = "*";
 
-// for(var increment = 1; increment<20; increment++) {
-//   console.log("*");
-//   pattern = pattern + symbols[increments];
-// }
+for (var increment = 0; increment < symbols.length; increment++) {
+    console.log(pattern);
+    pattern = pattern + symbols[increment];
+    var ampersand = "&";
+    console.log(increment);
+    if (increment > 5) {
+        for (var another = 0; another < 10; another++) {
+            console.log(ampersand);
+            ampersand = ampersand + "&";
+        }
+    }
+}
